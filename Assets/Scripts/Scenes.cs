b5421f4dfa8bfe63ba0f60ac750f2769f6302d23
9rey-m477er <sp378523@gmail.com>
2023-10-12 21:28:11 -0400

    Fixed loading screen
    Single Player Button loads Platform 1 and Two Players loads Platform 2

--- Assets/Scripts/Scenes.cs
+++ Assets/Scripts/Scenes.cs
@@ -5,11 +5,11 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 public class Scenes : MonoBehaviour
 {
-    public void SinglePlayer()
+   public void SinglePlayer()
     {
         SceneManager.LoadScene("Platform 1");
     }
-    public void TwoPlayer()
+   public void TwoPlayer()
     {
         SceneManager.LoadScene("Platform 2");
     }
