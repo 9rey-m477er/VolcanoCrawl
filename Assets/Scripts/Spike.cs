d45df07f154ca86e28a2982ae63ecde4e5fedef6
BrookeEbetino <bebetino17@yahoo.com>
2023-11-08 18:12:25 -0500

    Spike Stun

--- Assets/Scripts/Spike.cs
+++ Assets/Scripts/Spike.cs
@@ -5,28 +5,68 @@ using UnityEngine.SceneManagement;
 
 public class Spike : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private bool isStunned = false;
+    private float stunDuration = 1.0f;
+    private float stunTimer = 0.0f;
+
+    private Rigidbody2D playerRigidbody; // Reference to the player's Rigidbody2D component.
+
+    private void Start()
     {
-        
+        // Find and store a reference to the player's Rigidbody2D component.
+        playerRigidbody = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "Player")
+        if (isStunned)
         {
-            Debug.Log("Dead");
-            Die();
+            stunTimer += Time.deltaTime;
+
+            if (stunTimer >= stunDuration)
+            {
+                isStunned = false;
+                stunTimer = 0.0f;
+
+                // Allow the player to move horizontally again when the stun duration is over.
+                EnableHorizontalMovement();
+            }
         }
     }
-    void Die()
-    {
 
-        SceneManager.LoadScene("LeaderBoard");
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && !isStunned)
+        {
+            Debug.Log("Stunned");
+            StunPlayer();
+        }
+    }
+
+    void StunPlayer()
+    {
+        isStunned = true;
+        // Restrict the player's horizontal movement while still allowing them to fall.
+        DisableHorizontalMovement();
+    }
+
+    void DisableHorizontalMovement()
+    {
+        if (playerRigidbody != null)
+        {
+            Vector2 currentVelocity = playerRigidbody.velocity;
+            currentVelocity.x = 0; // Set horizontal velocity to zero while preserving vertical velocity.
+            playerRigidbody.velocity = currentVelocity;
+        }
+    }
+
+    void EnableHorizontalMovement()
+    {
+        if (playerRigidbody != null)
+        {
+            // Restore the player's ability to move horizontally.
+
+        }
     }
 }
