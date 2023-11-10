c5ca7446f7d2e965074f39d067fb0fbfa5b0a9d0
Reecepter <rfreem57@students.kennesaw.edu>
2023-11-10 18:56:41 -0500

    Performance update
    Added Crystal Level
    Added pop-up text for crystals
    Added Fake Lights and Tweaked Particles for better performance

--- Assets/Scripts/ItemCollector.cs
+++ Assets/Scripts/ItemCollector.cs
@@ -10,8 +10,8 @@ public class ItemCollector : MonoBehaviour
 
     public AudioClip coinCollectSound;
     AudioSource audioSource;
-    
 
+    [SerializeField] private GameObject floatingPoints;
     private void Start()
     {
         audioSource= GetComponent<AudioSource>();
@@ -21,9 +21,11 @@ public class ItemCollector : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Coin"))
         {
+            ShowPoints(collision.gameObject, "+10");
             ParticleSystem ps = collision.gameObject.GetComponentInChildren<ParticleSystem>();
             ps.Play();
             collision.gameObject.GetComponent<Renderer>().enabled = false;
+            collision.gameObject.GetComponent<BoxCollider2D>().enabled= false;
             audioSource.PlayOneShot(coinCollectSound, 1f);
             coins++;
             coinText.text = "Crystals: " + coins;
@@ -31,6 +33,14 @@ public class ItemCollector : MonoBehaviour
             scoreboard.AddToScore(10);
             //collision.gameObject.SetActive(false);
             Destroy(collision.gameObject, 0.25f);
+        }
+    }
+    void ShowPoints(GameObject crystalObj, string pointText)
+    {
+        if (floatingPoints)
+        {
+            GameObject ftPrefab = Instantiate(floatingPoints, crystalObj.transform.position, Quaternion.identity);
+            ftPrefab.GetComponentInChildren<TextMesh>().text = pointText;
         }
     }
 }
