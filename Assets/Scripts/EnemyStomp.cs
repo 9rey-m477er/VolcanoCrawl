19f0e3ef93384dfddf51a7c1cebb5367cb508608
Reecepter <rfreem57@students.kennesaw.edu>
2023-11-12 15:47:16 -0500

    11-12 Changes Reece

--- Assets/Scripts/EnemyStomp.cs
+++ Assets/Scripts/EnemyStomp.cs
@@ -4,12 +4,24 @@ using UnityEngine;
 
 public class EnemyStomp : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject floatingPoints;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         GameObject colGO = collision.gameObject;
         if (colGO.tag == "EnemyHead")
         {
+            ShowPoints(collision.gameObject, "+10");
             colGO.transform.parent.gameObject.SetActive(false);
         }
     }
+    void ShowPoints(GameObject crystalObj, string pointText)
+    {
+        if (floatingPoints)
+        {
+            GameObject ftPrefab = Instantiate(floatingPoints, crystalObj.transform.position, Quaternion.identity);
+            ftPrefab.GetComponentInChildren<TextMesh>().text = pointText;
+        }
+    }
+
 }
