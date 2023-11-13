7d6ac9f0308823e6606868e1c5f28f852db0025b
BrookeEbetino <bebetino17@yahoo.com>
2023-11-13 18:21:44 -0500

    2P Camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    [SerializeField]private Transform player;
-    
+    [SerializeField] private Transform player1;
+    [SerializeField] private Transform player2;
+
     private void Update()
     {
-        transform.position = new Vector3(transform.position.x,player.position.y,transform.position.z);
+            // Single player mode - follow Player1
+            if (player2 == null)
+            {
+                transform.position = new Vector3(transform.position.x, player1.position.y, transform.position.z);
+            }
+        
+        else
+        {
+            // Two-player mode - stay between Player1 and Player2
+            float averageY = (player1.position.y + player2.position.y) / 2f;
+            transform.position = new Vector3(transform.position.x, averageY, transform.position.z);
+        }
     }
 }
